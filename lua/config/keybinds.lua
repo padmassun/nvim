97e44212ddeb5607fb3fcc389538e6ca231094e4
padmassun <padmassun@gmail.com>
2025-10-13 01:43:25 -0400

    Added barbar, auto-session, and some keymaps

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,6 +1,19 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = false }
+
 vim.keymap.set("n", "<leader>f", function()
-  vim.lsp.buf.format()
+	vim.lsp.buf.format()
 end, { noremap = true, silent = true })
+
+-- Move to previous/next
+map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+
+-- Close buffer
+map("n", "<C-w>", "<Cmd>BufferClose<CR>", opts)
+
+-- Save buffer with Ctrl + s and stay in insert mode
+map("i", "<C-s>", "<C-o>:w<CR>", opts)
--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -1,16 +1,14 @@
 return {
-    { -- Git plugin
-	'tpope/vim-fugitive',
-    },
-    { -- Show historical versions of the file locally
-	'mbbill/undotree',
-    },
-    { -- Show CSS Colors
-	'brenoprata10/nvim-highlight-colors',
-	config = function()
-	    require('nvim-highlight-colors').setup({})
-	end
-    },
+	{ -- Git plugin
+		"tpope/vim-fugitive",
+	},
+	{ -- Show historical versions of the file locally
+		"mbbill/undotree",
+	},
+	{ -- Show CSS Colors
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
 }
-
-
